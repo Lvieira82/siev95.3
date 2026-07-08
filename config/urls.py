cb03293b9a329eb09a0d4a994f634d5c26bb710b
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-07-08 11:05:35 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,8 @@
-from django.views.static import serve
-from django.urls import re_path
-from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls.static import static
-from apps.solicitacoes.views import gerar_opo
+from django.urls import path, re_path
+from django.views.static import serve
+from django.conf import settings
+
 from apps.solicitacoes.views import (
     home,
     nova_solicitacao,
@@ -24,111 +22,118 @@ from apps.solicitacoes.views import (
     lancamento_manual,
     opos_geradas,
     detalhe_opo,
-    
-    
+    gerar_opo,
 )
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
 
-    path('admin/', admin.site.urls),
     path(
         "documento/<int:id>/<str:tipo>/",
         abrir_documento_solicitacao,
         name="abrir_documento_solicitacao",
     ),
+
     path(
         "gestao/lancamento-manual/",
         lancamento_manual,
-        name="lancamento_manual"
+        name="lancamento_manual",
     ),
+
     path(
         "gestao/opos-geradas/",
         opos_geradas,
-        name="opos_geradas"
+        name="opos_geradas",
     ),
 
     path(
         "gestao/opo/<int:id>/detalhes/",
         detalhe_opo,
-        name="detalhe_opo"
+        name="detalhe_opo",
     ),
-    path('', home, name='home'),
+
+    path("", home, name="home"),
+
     path(
         "gestao/agenda/",
         agenda_gestao,
-        name="agenda_gestao"
+        name="agenda_gestao",
     ),
-    path('gestao/', login_gestao, name='login_gestao'),
 
-    path('logout/', logout_gestao, name='logout_gestao'),
+    path("gestao/", login_gestao, name="login_gestao"),
+
+    path("logout/", logout_gestao, name="logout_gestao"),
 
     path(
-        'painel/',
+        "painel/",
         painel_gestao,
-        name='painel_gestao'
+        name="painel_gestao",
     ),
 
     path(
-        'consultar/',
+        "consultar/",
         consultar_protocolo,
-        name='consultar_protocolo'
+        name="consultar_protocolo",
     ),
+
     path(
         "documentos/<int:id>/",
         documentos_solicitacao,
-        name="documentos_solicitacao"
+        name="documentos_solicitacao",
     ),
 
     path(
-        'nova/',
+        "nova/",
         nova_solicitacao,
-        name='nova_solicitacao'
+        name="nova_solicitacao",
     ),
 
     path(
-        'minhas/',
+        "minhas/",
         minhas_solicitacoes,
-        name='minhas_solicitacoes'
+        name="minhas_solicitacoes",
     ),
 
     path(
-        'dashboard/',
+        "dashboard/",
         dashboard_operacional,
-        name='dashboard_operacional'
+        name="dashboard_operacional",
     ),
 
     path(
-        'verificar/<str:protocolo>/',
+        "verificar/<str:protocolo>/",
         verificar_autenticidade,
-        name='verificar_autenticidade'
+        name="verificar_autenticidade",
     ),
 
     path(
-        'alterar-status/<int:id>/<str:status>/',
+        "alterar-status/<int:id>/<str:status>/",
         alterar_status,
-        name='alterar_status'
+        name="alterar_status",
     ),
 
     path(
-        'aprovar/<int:id>/',
+        "aprovar/<int:id>/",
         aprovar_solicitacao,
-        name='aprovar_solicitacao'
+        name="aprovar_solicitacao",
     ),
+
     path(
         "opo/<int:id>/",
         gerar_opo,
-        name="gerar_opo"
+        name="gerar_opo",
     ),
+
     path(
         "aprovacoes/",
         listar_pendentes_opo,
-        name="listar_pendentes_opo"
+        name="listar_pendentes_opo",
     ),
-]
 
+    # Servir arquivos da pasta media
     re_path(
-        r'^media/(?P<path>.*)$',
+        r"^media/(?P<path>.*)$",
         serve,
-        {'document_root': settings.MEDIA_ROOT},
+        {"document_root": settings.MEDIA_ROOT},
     ),
 ]
