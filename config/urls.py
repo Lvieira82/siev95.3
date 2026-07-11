c3638193d8fd86d9cca1a99cea7581852d4bcca6
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-07-11 08:38:29 -0300

    url de ontem

--- config/urls.py
+++ config/urls.py
@@ -122,12 +122,12 @@ urlpatterns = [
         validar_matricula_opo_publica,
         name="validar_matricula_opo_publica"
     ),
-    
     path(
         "consulta/opo/<int:id>/detalhes/",
         detalhe_opo_publica,
         name="detalhe_opo_publica"
     ),
+
     path(
         "minhas/",
         minhas_solicitacoes,
