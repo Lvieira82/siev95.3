3c81b3dccec83107c7385b1dbf033793efcec59c
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-07-27 08:36:08 -0300

    Update enviar_pesquisas.py

--- apps/solicitacoes/enviar_pesquisas.py
+++ apps/solicitacoes/enviar_pesquisas.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        limite = timezone.now() - timedelta(hours=48)
+        limite = timezone.now() - timedelta(hours=24)
 
         solicitacoes = Solicitacao.objects.filter(
             status="APROVADO",
