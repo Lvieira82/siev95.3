d34ed73e4351d182337200b334b0eba847010434
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-07-29 10:22:06 -0300

    remover_documentos_antigos.py

--- apps/solicitacoes/management/commands/remover_documentos_antigos.py
+++ apps/solicitacoes/management/commands/remover_documentos_antigos.py
@@ -1,1 +1,42 @@
+from datetime import timedelta
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+from apps.solicitacoes.models import Solicitacao
 
+
+class Command(BaseCommand):
+    help = "Remove documentos de solicitações com mais de 7 dias"
+
+    def handle(self, *args, **kwargs):
+
+        limite = timezone.now() - timedelta(days=7)
+
+        solicitacoes = Solicitacao.objects.filter(
+            data_criacao__lt=limite
+        )
+
+        total = 0
+
+        for s in solicitacoes:
+
+            for campo in [
+                "oficio_comandante",
+                "documento_sanitario",
+                "documento_meio_ambiente",
+                "oficio_bombeiro",
+            ]:
+
+                arquivo = getattr(s, campo)
+
+                if arquivo:
+                    arquivo.delete(save=False)
+                    setattr(s, campo, None)
+
+            s.save()
+            total += 1
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"{total} solicitações processadas."
+            )
+        )
