6835dcc49173c7de3bf6021f7df45870a4e10645
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-07-30 08:14:22 -0300

    Update apps.py

--- apps/solicitacoes/apps.py
+++ apps/solicitacoes/apps.py
@@ -2,5 +2,16 @@ from django.apps import AppConfig
 
 
 class SolicitacoesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.solicitacoes'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.solicitacoes"
+
+    def ready(self):
+
+        from django.conf import settings
+
+        if settings.DEBUG:
+            return
+
+        from .scheduler import iniciar_scheduler
+
+        iniciar_scheduler()
